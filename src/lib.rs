c1bc10fef846c77cf02cf259da5349d3647bff9b
chux0519 <chuxdesign@hotmail.com>
2019-05-01 10:03:57 +0800

    feat: add q026

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ mod q001_two_sum;
 mod q023_merge_k_lists;
 mod q024_swap_nodes_in_pairs;
 mod q025_reverse_nodes_in_k_group;
+mod q026_remove_duplicates_from_sorted_array;
---
+++ src/q026_remove_duplicates_from_sorted_array.rs
@@ -0,0 +1,29 @@
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.len() == 0 || nums.len() == 1 {
+            return nums.len() as i32;
+        }
+        let (mut i, mut j) = (0, 1);
+        while i < nums.len() && j < nums.len() {
+            if nums[i] != nums[j] {
+                i += 1;
+                nums[i] = nums[j];
+            }
+            j += 1;
+        }
+        (i + 1) as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn q026() {
+        assert_eq!(0, Solution::remove_duplicates(&mut vec![]));
+        assert_eq!(2, Solution::remove_duplicates(&mut vec![1, 1, 2]));
+        assert_eq!(5, Solution::remove_duplicates(&mut vec![0,0,1,1,1,2,2,3,3,4]));
+    }
+}
