8814290fe1967f9f77e673e9150d89265666e170
chux0519 <chuxdesign@hotmail.com>
2020-02-10 12:08:23 +0800

    fix: q029
    i64 is forbidden

--- src/q029_divide_two_integers.rs
+++ src/q029_divide_two_integers.rs
@@ -1,7 +1,6 @@
 struct Solution;
 
 impl Solution {
-    /// 使用加减法即可，同时将 divisor 进行位移运算以加速计算
     pub fn divide(dividend: i32, divisor: i32) -> i32 {
         if divisor == 1 {
             return dividend;
@@ -10,12 +9,15 @@ impl Solution {
             return i32::max_value();
         }
         let minus = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
-        let (mut dvd, dvs) = (i64::abs(dividend as i64), i64::abs(divisor as i64));
+        // avoid abs here, turned dvd and dvs to negative
+        // cause abs(i32::min_value()) > abs(i32::max_value())
+        let mut dvd = if dividend < 0 { dividend } else { -dividend };
+        let dvs = if divisor < 0 { divisor } else { -divisor };
         let mut ret = 0;
         let mut shift = 0;
         let mut sub = dvs;
-        while dvd >= dvs {
-            if dvd >= sub {
+        while dvd <= dvs {
+            if dvd <= sub {
                 dvd -= sub;
                 ret += 1 << shift;
                 sub <<= 1;
@@ -43,5 +45,6 @@ mod tests {
         assert_eq!(-2, Solution::divide(7, -3));
         assert_eq!(2147483647, Solution::divide(-2147483648, -1));
         assert_eq!(-2147483648, Solution::divide(-2147483648, 1));
+        assert_eq!(-1073741824, Solution::divide(-2147483648, 2))
     }
 }
